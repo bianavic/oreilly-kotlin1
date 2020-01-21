e5981c150e631ce71d035966796bed28ff31ef09
bianavic <bianavic@gmail.com>
2020-01-21 14:32:31 -0300

    result is excellent

--- src/com/hadihariri/kotlincourse/basics/Conditionals.kt
+++ src/com/hadihariri/kotlincourse/basics/Conditionals.kt
@@ -1,17 +1,28 @@
 package com.hadihariri.kotlincourse.basics
 
-fun main(args: Array<String>) {
+//fun main(args: Array<String>) {
+//
+//    var myString = "Not Empty"
+//
+//    // if <if> is a expression so i can assign a val
+//    // IF conditional is TRUE will RETURN a number (20) else will return a STRING
+//    val result = if (!myString.equals("")) {
+//        // it s casting this expression to ANY
+//        println("Not Empty")
+//    }
 
-    val myString = "Not Empty"
 
-    // if <if> is a expression so i can assign a val
-    // IF conditional is TRUE will RETURN a number (20) else will return a STRING
-    val result = if (!myString.equals("")) {
-        // it s casting this expression to ANY
-        "Not Empty"
-        20
-    } else {
-        "Is Empty" // kotlin DOESNT return LAST VALUE
+//        20
+//    } else {
+//        "Is Empty" // kotlin DOESNT return LAST VALUE
+//    }
+//    println(result)
+
+    // WHEN = the result can have a value or a String and then execute that block
+    fun main(args: Array<String>) {
+        val result = "Value"
+        when (result) {
+            is String -> println("Excellent")
+//        "Value" -> println("It's a value")
     }
-    println(result)
 }
