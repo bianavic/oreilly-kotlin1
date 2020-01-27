2618c6b505719d1d217b2dc0dcbea0434059c8e1
bianavic <bianavic@gmail.com>
2020-01-27 13:30:07 -0300

    spread operator

--- src/com/hadihariri/kotlincourse/funtions/Functions.kt
+++ src/com/hadihariri/kotlincourse/funtions/Functions.kt
@@ -1,21 +1,5 @@
 package com.hadihariri.kotlincourse.funtions
 
-fun hello(): Unit {
-    println("Hello")
-}
-
-fun throwingException(): Nothing {
-    throw Exception("This function returns an exception")
-}
-
-fun returnAFour(): Int {
-    return 4
-}
-
-fun takingString(name: String) {
-    println(name)
-}
-
 // MULTIPLE PARAMETERS
 fun sum(x: Int, y: Int, z: Int = 0, w: Int = 0) = x + y + z + w
 
@@ -26,23 +10,36 @@ fun printDetails(name: String, email: String = "", phone: String) { // make Stri
 // kotlin has DEFAULT parameters.
 // so without declaring the third parameter
 // it will default it to zero
+
+// I can pass ANY number of parameters
+fun printStrings(vararg strings: String) {
+    reallyPrintingStrings(*strings) // SPREAD OPERATOR
+}
+
+// refactor > extract function
+private fun reallyPrintingStrings(vararg strings: String) {
+    for (string in strings) {
+        println(string)
+    }
+
+}
+
 fun sum(x: Int, y: Int) = x + y
 
 fun main(args: Array<String>) {
-
-    hello()
-
-    val value = returnAFour()
-
-    takingString("Some Value")
 
     // have multiple parameters but i will ONLY pass a specific one and not another
     sum(1, 2, 3)
     sum(1, 2)
 
     printDetails("Fabi", phone = "543 66 6") // passing the third parameter
-
     // it doesnt define the order of values, like reversing the order, changing the way we like
+
+
+    printStrings("1")
+    printStrings("1", "2")
+    printStrings("1", "2", "3")
+    printStrings("1", "2", "3", "4")
 
 }
 
