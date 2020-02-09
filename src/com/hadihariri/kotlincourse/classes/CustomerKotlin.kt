85e421d81c1a78f2ad24a7d9f872abf28753d101
bianavic <bianavic@gmail.com>
2020-02-09 09:39:53 -0300

    return json

--- src/com/hadihariri/kotlincourse/classes/CustomerKotlin.kt
+++ src/com/hadihariri/kotlincourse/classes/CustomerKotlin.kt
@@ -1,27 +1,41 @@
 package com.hadihariri.kotlincourse.classes
 
-data class CustomerKotlin (var id: Int, var name: String, var email: String)
+data class CustomerKotlin (var id: Int, var name: String, var email: String) {
+    override fun toString(): String {
 
-// DATA - avoid create boilplater code for toString, get hash, euqals code (a MODIFIER)
+        // return JSON (SCAPE lines)
+        // or USE INTELLIJ or edit injection to return JSON
+        return "{\"id\": \"$id\", \"name\": \"$name\"}"
+    }
+}
+
+// DATA - avoid create BOILERPLATE INTEROPERABILITY code for toString, get hash, equals code (a MODIFIER)
 
 fun main(args: Array<String>) {
+
 
     val customer1 = CustomerKotlin(1, "Fabi", "mail@mail.com")
     val customer2 = CustomerKotlin(1, "Fabi", "mail@mail.com")
 
-    val customer3 = customer1
+    // print as JSON
+    println(customer1)
+    println(customer2)
 
-    // println(customer)"
+
+/*        val customer3 = customer1
+    // println(customer)
+
+    // ability to COPY OBJECTS
     if (customer1 == customer2) {
         println("They are the same")
     }
 
-    // ability to copy objects
+    // COPY FUNCTIONS
     val customer4 = customer1.copy(email = "wire@wire.com")
     println(customer4.id)
     println(customer4.name)
     println(customer4.email)
 
-    // customer1.copy()
+    // customer1.copy()*/
 
 }
