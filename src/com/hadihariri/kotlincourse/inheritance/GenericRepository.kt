619725930a102953e536cf363cb1eddb1df07f11
bianavic <bianavic@gmail.com>
2020-02-11 10:52:05 -0300

    generics

---
+++ src/com/hadihariri/kotlincourse/inheritance/GenericRepository.kt
@@ -0,0 +1,31 @@
+package com.hadihariri.kotlincourse.inheritance
+
+import java.lang.UnsupportedOperationException
+
+// type parameters CLASS
+class GenericRepository<T> : Repository<T> {
+    override fun getById(id: Int): T {
+        throw UnsupportedOperationException("not implemented")
+    }
+    override fun getAll(): List<T> {
+        throw UnsupportedOperationException("not implemented")
+    }
+}
+
+interface Repo: {
+    fun<EntityType> getById(id: Int): EntityType
+    fun<R> getAll(): List<R> // single letters usually represent TYPE PARAMETERS
+}
+
+class MyRepo: Repo {
+
+    // type parameters FUNCTIONS
+    override fun <T> getById(id: Int): T {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun <T> getAll(): List<T> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
---
+++ src/com/hadihariri/kotlincourse/inheritance/Generics.kt
@@ -0,0 +1,14 @@
+package com.hadihariri.kotlincourse.inheritance
+
+interface Repository<T> {
+    fun getById(id: Int): T
+    fun getAll(): List<T>
+}
+
+fun main(args: Array<String>) {
+    // generic repository of type Customer
+    val customerRepo = GenericRepository<Customer>()
+
+    val customer = customerRepo.getById(10)
+    val customers = customerRepo.getAll()
+}
