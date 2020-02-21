b5db281d15b49c74c1d7b2c78839cfbb544eff27
bianavic <bianavic@gmail.com>
2020-02-21 16:23:12 -0300

    lambda expressions

--- src/com/hadihariri/kotlincourse/functional/GettingFunctional.kt
+++ src/com/hadihariri/kotlincourse/functional/GettingFunctional.kt
@@ -10,7 +10,9 @@ fun operation(x: Int, op: (Int) -> Unit) {}
 
 fun route(path: String, vararg actions: (String, String) -> String) {}
 
-// use that  creatin a fun that uses 'op fun
+fun unaryOperation(x: Int, op: (Int) -> Int) {}
+
+// use that creating a fun that uses 'op fun
 fun sum(x: Int, y: Int) = x + y
 
 fun main(args: Array<String>) {
@@ -18,5 +20,15 @@ fun main(args: Array<String>) {
     // lets call
     // we are passing the function sum to the function operation
     print( operation(1, 2, ::sum)) // reference a function by name with :: plus the name of the function
+    val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
+    operation(1, 2, sumLambda)
+
+    unaryOperation(2, { x -> x * x }) // correct
+    unaryOperation(3, { it * it}) // correct
+    unaryOperation(3) { it * it}
+
+    // anonymous function
+    unaryOperation(3, fun(x: Int): Int { return x * x })
+
 
 }
